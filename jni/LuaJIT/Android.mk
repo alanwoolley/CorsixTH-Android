5226b91b9dca7ec5005014bc1492e4a0da465a81
Alan Woolley <alan@armedpineapple.co.uk>
2013-01-19 17:59:58 +0000

    Bring back LuaJIT
    Don't include it yet though.

---
+++ jni/LuaJIT/Android.mk
@@ -0,0 +1,6 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE:= luajit
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libluajit.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/src $(LOCAL_PATH)/dynasm
+include $(PREBUILT_SHARED_LIBRARY)
