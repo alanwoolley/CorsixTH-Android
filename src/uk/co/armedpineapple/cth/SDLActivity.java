fd328c1f87d4b2065db8ff854033f38515f2c275
Alan Woolley <alan@armedpineapple.co.uk>
2013-03-02 12:14:36 +0000

    Fix getting stuck in paused mode

--- src/uk/co/armedpineapple/cth/SDLActivity.java
+++ src/uk/co/armedpineapple/cth/SDLActivity.java
@@ -750,8 +750,8 @@ public class SDLActivity extends CTHActivity {
 				case QUICK_SAVE:
 					cthSaveGame(context.getString(R.string.quicksave_name));
 					break;
-				case RESTART_GAME:
-					cthRestartGame();
+				case RESTART_GAME:	
+					cthRestartGame();				
 					break;
 
 				case SHOW_LOAD_DIALOG:
