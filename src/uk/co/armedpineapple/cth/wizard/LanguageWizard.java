11d75ec034caef0e0a9bd2882d06b6d84a8e6275
Alan Woolley <alan@armedpineapple.co.uk>
2013-03-07 22:20:25 +0000

    Revamp Language wizard page

--- src/uk/co/armedpineapple/cth/wizard/LanguageWizard.java
+++ src/uk/co/armedpineapple/cth/wizard/LanguageWizard.java
@@ -11,58 +11,150 @@ import uk.co.armedpineapple.cth.Configuration;
 import uk.co.armedpineapple.cth.ConfigurationException;
 import uk.co.armedpineapple.cth.R;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class LanguageWizard extends WizardView {
-
-	Spinner		languageSpinner;
 	Context		ctx;
-	String[]	langValuesArray;
-	String[]	langArray;
-
-	public LanguageWizard(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		ctx = context;
-	}
+	ListView	languageListView;
 
 	public LanguageWizard(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		ctx = context;
+
 	}
 
-	public LanguageWizard(Context context) {
-		super(context);
-		ctx = context;
+	@Override
+	protected void onFinishInflate() {
+		super.onFinishInflate();
+		if (!isInEditMode()) {
+			languageListView = (ListView) findViewById(R.id.language_list);
+			String[] langArray = getResources().getStringArray(R.array.languages);
+			String[] langValuesArray = getResources().getStringArray(
+					R.array.languages_values);
+			TypedArray langFlagsArray = getResources().obtainTypedArray(
+					R.array.languages_flags);
+			languageListView.setAdapter(new LanguageListAdapter(ctx, langArray,
+					langValuesArray, langFlagsArray));
+
+			languageListView.setOnItemClickListener(new OnItemClickListener() {
+
+				@Override
+				public void onItemClick(AdapterView<?> arg0, View arg1, int pos,
+						long arg3) {
+					((LanguageListAdapter) arg0.getAdapter()).setSelectedItem(pos);
+
+				}
+			});
+			// Look for the language in the values array
+			Log.d(getClass().getSimpleName(), "System Language: "
+					+ Locale.getDefault().getLanguage());
+
+			for (int i = 0; i < langValuesArray.length; i++) {
+				if (langValuesArray[i].equals(Locale.getDefault().getLanguage())) {
+					((LanguageListAdapter) languageListView.getAdapter())
+							.setSelectedItem(i);
+					break;
+				}
+
+			}
+		}
 	}
 
 	@Override
 	void saveConfiguration(Configuration config) throws ConfigurationException {
-		config.setLanguage(langValuesArray[languageSpinner
-				.getSelectedItemPosition()]);
+
+		String lang = (String) ((LanguageListAdapter) languageListView.getAdapter())
+				.getSelectedItem();
+
+		config.setLanguage(lang);
+
 	}
 
 	@Override
 	void loadConfiguration(Configuration config) {
-		languageSpinner = ((Spinner) findViewById(R.id.languageSpinner));
-		langValuesArray = ctx.getResources().getStringArray(
-				R.array.languages_values);
-		langArray = ctx.getResources().getStringArray(R.array.languages);
 
-		languageSpinner.setSelection(0);
+	}
 
-		// Look for the language in the values array
-		Log.d(getClass().getSimpleName(), "System Language: " + Locale.getDefault().getLanguage());
-		
-		for (int i = 0; i < langValuesArray.length; i++) {
-			if (langValuesArray[i].equals(Locale.getDefault().getLanguage())) {
-				languageSpinner.setSelection(i);
-				break;
-			}
-			
+	class LanguageListAdapter extends BaseAdapter {
+		String[]				text;
+		String[]				values;
+		TypedArray			resources;
+
+		LayoutInflater	inflater;
+		int							selected	= 0;
+
+		public LanguageListAdapter(Context ctx, String[] langArray,
+				String[] langValuesArray, TypedArray langFlagsArray) {
+			super();
+			text = langArray;
+			values = langValuesArray;
+			resources = langFlagsArray;
+			inflater = LayoutInflater.from(ctx);
+
 		}
 
+		@Override
+		public int getCount() {
+			return text.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return values[position];
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		public Object getSelectedItem() {
+			return getItem(selected);
+		}
+
+		public void setSelectedItem(int pos) {
+			selected = pos;
+			notifyDataSetChanged();
+		}
+
+		@Override
+		public View getView(final int position, View convertView, ViewGroup parent) {
+			View newView;
+
+			if (convertView == null) {
+				newView = inflater.inflate(R.layout.language_list_item, null);
+			} else {
+				newView = convertView;
+			}
+
+			((ImageView) newView.findViewById(R.id.language_flag))
+					.setImageDrawable(resources.getDrawable(position));
+			((TextView) newView.findViewById(R.id.language_text))
+					.setText(text[position]);
+
+			if (selected == position) {
+				((ImageView) newView.findViewById(R.id.language_tick))
+						.setVisibility(VISIBLE);
+			} else {
+				((ImageView) newView.findViewById(R.id.language_tick))
+						.setVisibility(INVISIBLE);
+			}
+
+			return newView;
+		}
 	}
 
 }
