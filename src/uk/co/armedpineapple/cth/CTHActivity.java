026c007f755bb03ca096a6b1ba5110c08131d21d
Alan Woolley <alan@armedpineapple.co.uk>
2013-08-27 17:01:22 +0100

    Remove Flurry and update Bugsense

--- src/uk/co/armedpineapple/cth/CTHActivity.java
+++ src/uk/co/armedpineapple/cth/CTHActivity.java
@@ -4,21 +4,14 @@
 //
 package uk.co.armedpineapple.cth;
 
-import java.util.Properties;
-import uk.co.armedpineapple.cth.R;
-
-import com.flurry.android.FlurryAgent;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 
 public abstract class CTHActivity extends Activity {
 
 	private static final String	LOG_TAG	= "CTHActivity";
-	
-	public CTHApplication	app;
-	boolean								trackingSession	= false;
+
+	public CTHApplication				app;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,38 +19,4 @@ public abstract class CTHActivity extends Activity {
 		app = (CTHApplication) getApplication();
 
 	}
-
-	@SuppressWarnings("nls")
-	@Override
-	protected void onStart() {
-		super.onStart();
-
-		Properties p = app.getProperties();
-
-		// Check if Flurry is enabled
-
-		if (p != null && p.containsKey("flurry.key")) {
-			Log.d(LOG_TAG, "Starting Flurry Session");
-			FlurryAgent.setCaptureUncaughtExceptions(Boolean.parseBoolean(p
-					.getProperty("flurry.catcherrors", "false")));
-			FlurryAgent.onStartSession(this, p.getProperty("flurry.key"));
-
-			// Log Flurry events if app.debug=true
-			FlurryAgent.setLogEnabled(app.debugMode);
-			trackingSession = true;
-		} else {
-			Log.d(LOG_TAG, "Flurry is not enabled");
-		}
-
-	}
-
-	@Override
-	protected void onStop() {
-		super.onStop();
-		if (trackingSession) {
-			FlurryAgent.onEndSession(this);
-		}
-
-	}
-
 }
