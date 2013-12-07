add41fd3bc71ff9326d611c74a0f509b7468a1b2
Alan Woolley <alan@armedpineapple.co.uk>
2013-12-07 12:52:28 +0000

    Add buildConfig information to the built APKs

--- src/Java/uk/co/armedpineapple/cth/CTHApplication.java
+++ src/Java/uk/co/armedpineapple/cth/CTHApplication.java
@@ -5,73 +5,66 @@
  */
 package uk.co.armedpineapple.cth;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-import uk.co.armedpineapple.cth.R;
-
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.bugsense.trace.BugSenseHandler;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class CTHApplication extends android.app.Application {
 
-	private static final String	APPLICATION_PROPERTIES_FILE	= "application.properties";
+    public static final String PREFERENCES_KEY = "cthprefs";
+    private static final String APPLICATION_PROPERTIES_FILE = "application.properties";
+    private static final String LOG_TAG = "CTHApp";
+    public Configuration configuration;
+    private SharedPreferences preferences;
+    private Properties properties = new Properties();
 
-	private static final String	LOG_TAG	= "CTHApp";
-	
-	public static final String	PREFERENCES_KEY	= "cthprefs";
-	private SharedPreferences		preferences;
-	public Configuration				configuration;
-	private Properties					properties			= new Properties();
-	public boolean							debugMode				= false;
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
-
-		preferences = getSharedPreferences(PREFERENCES_KEY, Context.MODE_PRIVATE);
+        preferences = getSharedPreferences(PREFERENCES_KEY, Context.MODE_PRIVATE);
 
 		/*
-		 * BugSense helps to discover bugs by reporting unhandled exceptions. If you
+         * BugSense helps to discover bugs by reporting unhandled exceptions. If you
 		 * want to use this, create a file called application.properties in the
 		 * assets folder and insert the line:
 		 * 
 		 * bugsense.key=<your API key>
 		 */
 
-		try {
-			InputStream inputStream = getAssets().open(APPLICATION_PROPERTIES_FILE);
-			Log.d(LOG_TAG, "Loading properties");
-			properties.load(inputStream);
-			debugMode = Boolean.parseBoolean(properties.getProperty("app.debug",
-					"false"));
-			setupBugsense();
+        try {
+            InputStream inputStream = getAssets().open(APPLICATION_PROPERTIES_FILE);
+            Log.d(LOG_TAG, "Loading properties");
+            properties.load(inputStream);
+            setupBugsense();
 
-		} catch (IOException e) {
-			Log.i(LOG_TAG, "No properties file found");
-		}
+        } catch (IOException e) {
+            Log.i(LOG_TAG, "No properties file found");
+        }
 
-	}
+    }
 
-	private void setupBugsense() {
-		if (properties.containsKey("bugsense.key") && !debugMode) {
-			Log.d(LOG_TAG, "Setting up bugsense");
-			BugSenseHandler.initAndStartSession(this,
-					(String) properties.get("bugsense.key"));
-		}
-	}
+    private void setupBugsense() {
+        if (properties.containsKey("bugsense.key") && BuildConfig.USE_BUGSENSE) {
+            Log.d(LOG_TAG, "Setting up bugsense");
+            BugSenseHandler.initAndStartSession(this,
+                    (String) properties.get("bugsense.key"));
+        }
+    }
 
-	public SharedPreferences getPreferences() {
-		return preferences;
+    public SharedPreferences getPreferences() {
+        return preferences;
 
-	}
+    }
 
-	public Properties getProperties() {
-		return properties;
-	}
+    public Properties getProperties() {
+        return properties;
+    }
 
 }
